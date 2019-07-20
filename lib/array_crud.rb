b340a627b9b7a8046ee7dc1d9a42944f5a800742
Christopher Dent <christopherdent01@gmail.com>
2019-07-20 21:38:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,6 +15,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  element = "test"
+  array = ["bread", "butter"]
+  array.pop("Oatmeal")
   
 end
 
