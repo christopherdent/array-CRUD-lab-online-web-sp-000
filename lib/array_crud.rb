783076397132498557638016b2e7a419409c6cc9
Christopher Dent <christopherdent01@gmail.com>
2019-07-20 21:54:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,16 +8,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  element = "arrays!"
-  pets = ["Chilito", "Calvin"]
-  pets << element 
+
+  array << element 
   
 end
 
 def add_element_to_start_of_array(array, element)
-
   array.unshift(element)
-  
 end
 
 def remove_element_from_end_of_array(array)
