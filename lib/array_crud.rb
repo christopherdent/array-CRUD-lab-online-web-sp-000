4341af7361eee3a699431a86e0b6723a58f92e98
Christopher Dent <christopherdent01@gmail.com>
2019-07-20 21:25:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,15 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  ["Hi", "I Am" ,"Mr.", "Array"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  
+  array = 
   
 end
 
